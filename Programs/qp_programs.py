66071526b4a2c8cee245f3dd876ffed0fa4c23ad
Akkshay Tandon <76265879+akkshayTandon@users.noreply.github.com>
2023-05-23 20:30:38 +0530

    add files via upload

---
+++ Programs/qp_programs.py
@@ -0,0 +1,60 @@
+'''
+write a program to read 2 floating point numbers. Add these numbers and assign the result to a integer.
+Display value of all three variables.
+'''
+
+# float_Num_1 = float(input("Enter a floating point number: "))
+# float_Num_2 = float(input("Enter another floating point number: "))
+# result_in_integer = int(float_Num_1 + float_Num_2)
+# print(f"float point number 1 : {float_Num_1}")
+# print(f"float point number 2 : {float_Num_2}")
+# print(f"result in integer : {result_in_integer}") 
+
+'''
+write a function that calculates and return the surface area and volume of sphere.
+'''
+
+# def sphere():
+#     radius = float(input("Enter the radius: "))
+#     surface_area = 4 * 3.14 * radius * radius
+#     volume = (4 * 3.14 * radius * radius * radius)/3
+#     return [surface_area,volume]
+
+# volSurfaceArea = sphere()
+# print(f"surface area of sphere = {volSurfaceArea[0]}")
+# print(f"volume of sphere = {volSurfaceArea[1]}")
+
+'''
+Find the area and perimeter(circumference) of a circle using function. Prompt the user for input.
+'''
+
+# def circle():
+#     radius = float(input("Enter the radius : "))
+#     area = 3.14 * radius * radius
+#     perimeter = 2 * 3.14 * radius #circumference
+#     return [area, perimeter]
+# areaPerimeter = circle()
+# print(f"area of circle = {areaPerimeter[0]}")
+# print(f"perimeter of circle = {areaPerimeter[1]}")
+
+'''
+write a program to take a string from user and count number of vowels present in it.
+'''
+
+# def count_Vowels():
+#     inputString = (input("Enter a string : ")).lower()
+#     count = 0
+#     for ch in inputString : 
+#         if(ch == 'a' or ch == 'e' or ch == 'i' or ch == 'o' or ch == 'u'):
+#             count = count + 1
+#     print(f"Number of vowels = {count}")
+# count_Vowels()
+
+'''
+write a program to print and store squares of numbers in dictionary.
+'''
+# number = int(input("Please enter the maximum number : "))
+# myDict = {}
+# for x in range(1, number + 1):
+#     myDict[x] = x**2
+# print("\nDictionary = ", myDict) 
